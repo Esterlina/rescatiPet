6db459b4a72d5e0c3791ad7b0bf95bb82e237bc2
Esterlina <natalia.guzman.s@usach.cl>
2019-12-15 22:07:43 -0300

    editar datos usuario

--- src/screens/Rescue/RescueScreen.js
+++ src/screens/Rescue/RescueScreen.js
@@ -175,6 +175,19 @@ class RescueScreen extends React.Component {
           </TouchableOpacity>
         </View>
       :null}
+      {type == "Fundacion" || type == "Rescatista"? 
+        <View style={[appStyle.lineTop,appStyle.lineBottom,]}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Settings')}>
+            <View style={{flexDirection:'row',padding:10,alignItems:'center'}}>
+              <Image
+                source={require('../../icons/rescue/ajustes.png')}
+                style= {{width:35,height:35,marginRight:10}}
+              />
+              <Text style={appStyle.textSemiBold}>Configurar mis datos</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      :null}
     </View>
     )
   }
